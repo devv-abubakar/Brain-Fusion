983e72e57466dbc61dccd51dff88710fd68ab6df
devva <devv.abubakar@gmail.com>
2024-01-14 21:50:09 +0500

    Update UI for Splash Activity

---
+++ app/src/main/java/devv/abubakar/brainfusion/Splash.kt
@@ -0,0 +1,11 @@
+package devv.abubakar.brainfusion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
