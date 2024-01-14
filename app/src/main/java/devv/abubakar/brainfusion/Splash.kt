b372e5d27b23c8c20fc59fd412b6a76f3d753466
devva <devv.abubakar@gmail.com>
2024-01-14 22:03:14 +0500

    Update UI for Splash Activity

--- app/src/main/java/devv/abubakar/brainfusion/Splash.kt
+++ app/src/main/java/devv/abubakar/brainfusion/Splash.kt
@@ -1,11 +1,23 @@
+@file:Suppress("DEPRECATION")
+
 package devv.abubakar.brainfusion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash : AppCompatActivity() {
+    private val splashDelay: Long = 3500
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+
+            val mainIntent = Intent(this@Splash, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, splashDelay)
     }
 }
